910fa74259ea54b5343ac9c44abed8d6ede9b475
Monicatoep <69761812+Monicatoep@users.noreply.github.com>
2024-11-29 19:51:07 +0100

    Changed globalization in REST to false, now migrations work again. Created Photos table in DB

---
+++ SeasonalStory/Migrations/20241129183533_SeasonalStory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeasonalStory.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeasonalStory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhotoSeason = table.Column<int>(type: "int", nullable: false),
+                    PhotoTemp = table.Column<int>(type: "int", nullable: false),
+                    Billede = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
--- SeasonalStory/Photo.cs
+++ SeasonalStory/Photo.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 using System.ComponentModel;
-using System.Globalization;
 
 
 namespace SeasonalStory
