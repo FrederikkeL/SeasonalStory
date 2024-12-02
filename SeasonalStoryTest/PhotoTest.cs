0ce20e08c85e152cbf402d43f7fd4e77fcda424f
Monicatoep <69761812+Monicatoep@users.noreply.github.com>
2024-12-02 10:34:16 +0100

    You can now add images to the database.

--- SeasonalStoryTest/PhotoTest.cs
+++ SeasonalStoryTest/PhotoTest.cs
@@ -6,15 +6,14 @@ namespace SeasonalStoryTest
     [TestClass]
     public class PhotoTest
     {
-        //Photo validPhoto = new Photo() { PhotoTemp = Temperature.BelowZero, PhotoSeason = Season.Winter, Image = new byte[] {1, 1, 1, 1, 1, 2 } };
-        //Photo UploadedImageNull = new Photo() { PhotoTemp = Temperature.BelowZero, PhotoSeason = Season.Winter, UploadedImage = null};
-        //Photo ImageNull = new Photo() { PhotoTemp = Temperature.BelowZero, PhotoSeason = Season.Winter, Image = null };
+        Photo validPhoto = new Photo() { PhotoTemp = TemperatureIntervals.BelowZero, PhotoSeason = Season.Winter, UploadedImage = new byte[] { 1, 1, 1, 1, 1, 2 } };
+        Photo ImageNull = new Photo() { PhotoTemp = TemperatureIntervals.BelowZero, PhotoSeason = Season.Winter, UploadedImage = null };
 
-        //[TestMethod]
-        //public void ValidateUploadedImageTest()
-        //{
-        //    Assert.ThrowsException<NullReferenceException>(() => UploadedImageNull.ValidateUploadedImage());
-        //    Assert.ThrowsException<NullReferenceException>(() => ImageNull.ValidateImage());
-        //}
+        [TestMethod]
+        public void ValidateTest()
+        {
+            validPhoto.Validate();
+            Assert.ThrowsException<NullReferenceException>(() => ImageNull.Validate());
+        }
     }
 }
