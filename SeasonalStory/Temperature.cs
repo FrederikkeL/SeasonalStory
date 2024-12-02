f104d206d48125bcd75e6785ba191425fc6c16fc
NannaRister <nannarister@hotmail.com>
2024-12-02 10:43:45 +0100

    Validatemetoder inde i temperaturklassen
    Tilføjet temperaturTestklasse og lavet test på Value

--- SeasonalStory/Temperature.cs
+++ SeasonalStory/Temperature.cs
@@ -8,4 +8,26 @@ public class Temperature
     public int Id { get; set; }
     public int Value { get; set; }
     public DateTime Timestamp { get; set; }
+
+    public Temperature(int value)
+    {
+        Value = value;
+    }
+
+    public Temperature()
+    {
+    }
+
+    public void ValidateValue()
+    {
+        if (Value < -30 || Value > 40)
+            throw new ArgumentOutOfRangeException("Invalid temperature");
+    }
+
+
+    public void Validate()
+    {
+        ValidateValue();   
+    }
+  
 }
---
+++ SeasonalStoryTest/TemperatureTests/TemperatureTest.cs
@@ -0,0 +1,57 @@
+﻿using SeasonalStory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeasonalStoryTest.TemperatureTests
+{
+    [TestClass]
+    public class TemperatureTest
+    {
+        Temperature validTemperature = new Temperature() { Value = 13 };
+        Temperature temperatureTooLow = new Temperature() { Value = -31 };
+        Temperature temperatureTooHigh = new Temperature() { Value = 41 };
+
+        [TestMethod]
+        public void ValidateValueTest()
+        {
+            validTemperature.ValidateValue();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => temperatureTooLow.ValidateValue());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => temperatureTooHigh.ValidateValue());
+        }
+
+        [TestMethod]
+        public void ValidateTest()
+        {
+            validTemperature.Validate();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => temperatureTooLow.ValidateValue());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => temperatureTooHigh.ValidateValue());
+        }
+
+        [TestMethod]
+        [DataRow(-30)]
+        [DataRow(0)]
+        [DataRow(40)]
+
+        public void LegalValueTest(int value)
+        {
+            Temperature temperature = new Temperature(value);
+            temperature.Validate();
+        }
+
+        [TestMethod]
+        [DataRow(-31)]
+        [DataRow(41)]
+
+        public void IlegalValueTest(int value)
+        {
+            Temperature temperature = new Temperature(value);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => temperature.Validate());
+
+        }
+
+    }
+}
