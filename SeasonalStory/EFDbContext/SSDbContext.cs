6bdccce35d94c1399844e9d2a28cbd89a566ba15
Emma Hyttel <emhyttel@gmail.com>
2024-12-02 10:01:47 +0100

    namespace

--- SeasonalStory/EFDbContext/SSDbContext.cs
+++ SeasonalStory/EFDbContext/SSDbContext.cs
@@ -1,22 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SeasonalStory.EFDbContext
+namespace SeasonalStory.EFDbContext;
+
+public class SSDbContext : DbContext
 {
-    public class SSDbContext : DbContext
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseSqlServer(@"data source=Emmas-PC;initial catalog=myDatabase;trusted_connection=true;trustServerCertificate=true");
-        }
-
-        public DbSet<Photo> Photos { get; set; }
-
-        public DbSet<Temperature> Temperatures { get; set; }
+        options.UseSqlServer(@"data source=Emmas-PC;initial catalog=myDatabase;trusted_connection=true;trustServerCertificate=true");
     }
+
+    public DbSet<Photo> Photos { get; set; }
+
+    public DbSet<Temperature> Temperatures { get; set; }
 }
